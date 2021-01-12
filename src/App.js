9eb91635029469e443e8927879b13ecd17f273f9
Matheus Foganholo <matheusfoganholo@hotmail.com>
2021-01-12 01:41:57 -0300

    Setting up routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+import Footer from './Components/Footer';
 
 const App = () => {
-  const helloWorld = 'Hello World';
   return (
     <React.Fragment>
-      <h1>{helloWorld}</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </React.Fragment>
   );
 };
