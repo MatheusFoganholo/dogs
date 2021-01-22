5bd6cd6970efdb8a81214f0f3f0ff77bd268c7a9
Matheus Foganholo <matheusfoganholo@hotmail.com>
2021-01-22 00:05:16 -0300

    Changing button type

--- src/Components/Forms/Button.js
+++ src/Components/Forms/Button.js
@@ -3,7 +3,7 @@ import styles from './Button.module.css';
 
 const Button = ({ children, ...props }) => {
   return (
-    <button {...props} className={styles.button} type="button">
+    <button {...props} className={styles.button} type="submit">
       {children}
     </button>
   );
