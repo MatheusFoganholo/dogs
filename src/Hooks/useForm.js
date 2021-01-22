85056fa27cd7d530313c4fe72faa0d9559bae9d3
Matheus Foganholo <matheusfoganholo@hotmail.com>
2021-01-22 00:05:45 -0300

    Creating hook useForm

---
+++ src/Hooks/useForm.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const types = {
+  email: {
+    regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: 'Preencha um e-mail válido.',
+  },
+};
+
+const useForm = (type) => {
+  const [value, setValue] = React.useState('');
+  const [error, setError] = React.useState(null);
+
+  function validate(inputType) {
+    if (type === false) return true;
+    if (inputType.length === 0) {
+      setError('Preencha um valor.');
+      return false;
+    } else if (types[type] && !types[type].regex.test(inputType)) {
+      setError(types[type].message);
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
+
+  function onChange({ target }) {
+    if (error) validate(target.value);
+    setValue(target.value);
+  }
+
+  return {
+    value,
+    setValue,
+    onChange,
+    error,
+    validate: () => validate(value),
+    onBlur: () => validate(value),
+  };
+};
+
+export default useForm;
