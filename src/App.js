8283be6b8594a38c6a489aa32170cdaaf7b0fcf4
Matheus Foganholo <matheusfoganholo@hotmail.com>
2021-01-25 21:34:04 -0300

    Creating protected route (account)

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Home from './Components/Home';
 import Login from './Components/Login/Login';
 import Footer from './Components/Footer';
 import { UserStorage } from './UserContext';
+import User from './Components/User/User';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
 
 const App = () => {
   return (
@@ -16,6 +18,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login/*" element={<Login />} />
+            <ProtectedRoute path="/account/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>
