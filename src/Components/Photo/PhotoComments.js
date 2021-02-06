2f68b897b9381857ef8d397c07032b8fc2d10a99
Matheus Foganholo <matheusfoganholo@hotmail.com>
2021-02-06 20:06:24 -0300

    Creating comment post

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { UserContext } from '../../UserContext';
+import PhotoCommentsForm from './PhotoCommentsForm';
+import styles from './PhotoComments.module.css';
 
-const PhotoComments = () => {
-  return <div>Comments</div>;
+const PhotoComments = ({ id, comments }) => {
+  const [commentsList, setCommentsList] = React.useState(() => comments);
+  const { login } = React.useContext(UserContext);
+
+  return (
+    <>
+      <ul className={styles.comments}>
+        {commentsList.map((comment) => (
+          <li key={comment.comment_ID}>
+            <strong>{comment.comment_author}:</strong>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={id} setComments={setCommentsList} />}
+    </>
+  );
 };
 
 export default PhotoComments;
